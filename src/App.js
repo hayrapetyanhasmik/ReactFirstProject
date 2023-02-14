bb64849210ebce15593e13860d720a42f129e042
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2023-02-14 11:33:03 +0400

    Added new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Header from './Header';
+import Container from './Container';
+import Description from './Description';
+import Footer from './Footer';
+
 
 function App() {
   return (
     <div className="App">
-
+        <Header />
+        <Container />
+        <Description />
+        <Footer />
     </div>
   );
 }
